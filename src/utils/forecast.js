2568a4d9d94cbd2725422d8c272ca09d2244c69c
Yasmin <yasminsdcastro@gmail.com>
2021-10-14 16:00:23 -0300

    refactor: callback to error

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -5,10 +5,13 @@ const forecast = (latitude, longitude, callback) => {
   const url = `http://api.weatherstack.com/current?access_key=9d35a028587b06197804c8180334ab07&query=${latitude},${longitude}`;
 
   request({ url, json: true }, (error, { body }) => {
+    console.log(body);
     if (error) {
       callback("Unable to connect to weather service!", undefined);
     } else if (body.error) {
       callback("Unable to find location", undefined);
+    } else if (body.sucess) {
+      callback(body.error.info, undefined);
     } else {
       const weather_description =
         body.current.weather_descriptions[0].toLowerCase();
